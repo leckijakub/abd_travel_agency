06e2d30dea77aff252ddc9192300ac6af7a23349
Z1bby <31073873+Z1bby@users.noreply.github.com>
2021-11-28 15:13:29 +0100

    updated query 1 in flask_test.py

--- app/tests/flask_test.py
+++ app/tests/flask_test.py
@@ -27,17 +27,17 @@ def test_start():
     print(f"\n\n{bcolors.OKCYAN}Adding some users, an offer, and a reservation:\n{bcolors.ENDC}")
 
     emp1 = Employee(uid="emp1", email="b@b", password=generate_password_hash("a", method='sha256'),name="Mark",surname="Smith", position=employee_type.Administrator)
-    emp2 = Employee(uid="emp2", email="b1@b", password=generate_password_hash("a", method='sha256'),name="DC",surname="Joker", position=employee_type.Animator)
+    emp2 = Employee(uid="emp2", email="b1@b", password=generate_password_hash("a", method='sha256'),name="John",surname="Constantine", position=employee_type.Animator)
     emp3 = Employee(uid="emp3", email="b2@b", password=generate_password_hash("a", method='sha256'),name="Bruce",surname="Wayne a.k.a. Batman", position=employee_type.Service_organizer)
-    emp4 = Employee(uid="emp4", email="b3@b", password=generate_password_hash("a", method='sha256'),name="Magnus",surname="Carlsen", position=employee_type.Reservation_empolyee)
+    emp4 = Employee(uid="emp4", email="b3@b", password=generate_password_hash("a", method='sha256'),name="John",surname="Wick", position=employee_type.Reservation_empolyee)
     db.session.add_all([emp1, emp2, emp3, emp4])
     db.session.commit()
 
     client1 = Client(uid="1", email="a@a", password=generate_password_hash("a", method='sha256'),name="Bob", surname="Smith", address="tu",phone_number="666 666 666")
     client2 = Client(uid="2", email="a1@a", password=generate_password_hash("a", method='sha256'),name="Keanu", surname="Reeves", address="Warszawa",phone_number="420 691 337")
-    client3 = Client(uid=emp3.uid, email="b3@bprivate", password=generate_password_hash("a", method='sha256'),name=emp3.name, surname=emp3.surname, address="Warszawa",phone_number="691 337 420")
-    client4 = Client(uid="4", email="a3@a", password=generate_password_hash("a", method='sha256'),name="John", surname="Wick", address="Warszawa",phone_number="133 742 069")
-    client5 = Client(uid="5", email="a4@a", password=generate_password_hash("a", method='sha256'),name="John", surname="Constantine", address="Warszawa",phone_number="213 769 420")
+    client3 = Client(uid=emp3.uid, email="b2@bprivate", password=generate_password_hash("a", method='sha256'),name=emp3.name, surname=emp3.surname, address="Warszawa",phone_number="691 337 420")
+    client4 = Client(uid=emp2.uid, email="b1@bprivate", password=generate_password_hash("a", method='sha256'),name=emp2.name, surname=emp2.surname, address="Warszawa",phone_number="133 742 069")
+    client5 = Client(uid=emp4.uid, email="b3@bprivate", password=generate_password_hash("a", method='sha256'),name=emp4.name, surname=emp4.surname, address="Warszawa",phone_number="213 769 420")
     db.session.add_all([client1, client2, client3, client4, client5])
 
     offer1 = Travel_agency_offer(uid=1, transport='z buta', accommodation='pod drzewem', event='marsz',organizer_id=None)
@@ -51,17 +51,20 @@ def test_start():
     jobj = json.loads(jfile.read())
     offers = db.metadata.tables['Travel_agency_offer']
     reservations = db.metadata.tables['Reservation']
-    employees = db.metadata.tables['Employee']
 
     from sqlalchemy import select, literal
 
     kobyla = reservations.insert().from_select(
         ['client_id', 'price', 'status', 'offer_id'],
         select(
-            employees.c.id,
+            Client.id,
             literal("0"),
             literal("accepted"),
-            offers.insert().values(**jobj).returning(Travel_agency_offer.id).cte('distinct_query')
+            offers.insert().values(**jobj).returning(Travel_agency_offer.id).cte('insert_query')
+        ).select_from( # important
+            Client
+        ).join(
+            Employee, Client.uid == Employee.uid
         )
     )
 
@@ -71,16 +74,12 @@ def test_start():
     db.session.commit()
 
     print(f"\n\n{bcolors.OKCYAN}Querying users and offers for new reservations:{bcolors.ENDC}\n")
-    users = User.query.all()
-    offers = Travel_agency_offer.query.all()
 
-    print(f"\n\n{bcolors.OKCYAN}Adding more reservations:{bcolors.ENDC}\n")
-    db.session.add_all([
-        Reservation(price=444, status='test offer', client_id=users[1].id, employee_id = None, offer_id = offers[0].id),
-        Reservation(price=444, status='test offer', client_id=users[2].id, employee_id = None, offer_id = offers[1].id),
-        Reservation(price=444, status='test offer', client_id=users[3].id, employee_id = None, offer_id = offers[1].id),
-        Reservation(price=444, status='test offer', client_id=users[4].id, employee_id = None, offer_id = offers[1].id)
-    ])
+    keanu = Client.query.where(Client.name == "Keanu").first()
+    marsz = Travel_agency_offer.query.where(Travel_agency_offer.event == "marsz").first()
+
+    print(f"\n\n{bcolors.OKCYAN}Adding one more reservation:{bcolors.ENDC}\n")
+    db.session.add(Reservation(price=444, status='test offer', client_id=keanu.id, employee_id = None, offer_id = marsz.id))
     db.session.commit()
 
     print(f"\n{bcolors.OKGREEN}Querying reservations:{bcolors.ENDC}\n")
@@ -90,12 +89,12 @@ def test_start():
 
     print(f"\n{bcolors.OKGREEN}Querying offers:{bcolors.ENDC}\n")
     offers = db.session.query(Travel_agency_offer).all()
-    print(f"\n{bcolors.OKGREEN}Offers result:\n{bcolors.ENDC}\n")
+    print(f"\n{bcolors.OKGREEN}Offers result:\n{bcolors.ENDC}")
     [print(offer.serialize) for offer in offers]
 
     print(f"\n{bcolors.OKGREEN}Querying clients:{bcolors.ENDC}\n")
-    clients = db.session.query(Client).filter(Client.address == 'Warszawa').all()
-    print(f"\n{bcolors.OKGREEN}Clients result:\n{bcolors.ENDC}\n")
+    clients = db.session.query(Client).all()
+    print(f"\n{bcolors.OKGREEN}Clients result:\n{bcolors.ENDC}")
     [print(client.serialize) for client in clients]
 
     from sqlalchemy import func
@@ -116,4 +115,22 @@ def test_start():
     ).order_by(func.count(Client.id).desc()).first()
 
     print(f"\n{bcolors.FAIL}Most popular offer among clients living in Warsaw: {bcolors.ENDC}" + str(bestoffer.serialize) + '\n')
+
+    bestoffer = db.session.query(
+        Client.name,
+        Client.surname,
+        Travel_agency_offer.accommodation,
+        Reservation.id
+    ).select_from(
+        Client
+    ).filter(
+        Client.address == 'Warszawa'
+    ).join(
+        Reservation, Client.id == Reservation.client_id
+    ).join(
+        Travel_agency_offer, Reservation.offer_id == Travel_agency_offer.id
+    ).all()
+    
+    print(f"\n{bcolors.FAIL}List of offers and clients living in Warsaw: {bcolors.ENDC}")
+    [print(row) for row in bestoffer]
 
