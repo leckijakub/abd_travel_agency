25ea75663eef69b2a149720bc741a3c395ad4098
Jakub Lecki <lec.jakub@gmail.com>
2021-11-28 17:39:51 +0100

    app: tests: flask_test.py: add ORM and SQL test
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

--- app/tests/flask_test.py
+++ app/tests/flask_test.py
@@ -7,6 +7,11 @@ from app.models.employee import Employee, employee_type
 from app.models.travel_agency_offer import Travel_agency_offer
 from werkzeug.security import generate_password_hash
 import json
+from urllib.parse import urlparse
+import psycopg2
+import os
+import time
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -20,7 +25,26 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def test_start():
+    test_init()
+    orm1_time, orm2_time = test_orm()
+    test_init()
+    sql1_time, sql2_time = test_sql()
+
+    print(f"\n\n{bcolors.HEADER}#################### TEST SUMAMRY ####################\n{bcolors.ENDC}")
+    print(f"""
+    ---------------------------------------------
+    |QUERY|       ORM        |       SQL        |
+    |*****|******************|******************|
+    |  1  | {str(orm1_time)[:16]} | {str(sql1_time)[:16]} |
+    |-----|------------------|------------------|
+    |  2  | {str(orm2_time)[:16]} | {str(sql2_time)[:16]} |
+    ---------------------------------------------
+    """)
+
+
+def test_init():
     print(f"\n{bcolors.WARNING}WARNING: Testing database... may result in fatal errors...{bcolors.ENDC}\n")
+    db.session.commit()
     db.drop_all()
     db.create_all()
     db.session.commit()
@@ -46,6 +70,7 @@ def test_start():
     db.session.add(Reservation(price=444, status='test offer', client_id=Client.query.first().id, employee_id = None, offer_id = offer1.id))
     db.session.commit()
 
+def test_orm():
     #json z pliku + nazwy tablic do insertowania ze zwracaniem kolumn
     jfile = open("tests/offer.json", "r")
     jobj = json.loads(jfile.read())
@@ -54,7 +79,7 @@ def test_start():
 
     from sqlalchemy import select, literal
 
-    kobyla = reservations.insert().from_select(
+    query1 = reservations.insert().from_select(
         ['client_id', 'price', 'status', 'offer_id'],
         select(
             Client.id,
@@ -69,9 +94,12 @@ def test_start():
     )
 
     print(f"\n\n{bcolors.HEADER}#################### QUERY 1 ####################\n{bcolors.ENDC}")
-    print(str(kobyla)+"\n\n\n")
-    db.session.execute(kobyla)
+
+    print(str(query1)+"\n\n\n")
+    t0 = time.time()
+    db.session.execute(query1)
     db.session.commit()
+    t1 = time.time() - t0
 
     print(f"\n\n{bcolors.OKCYAN}Querying users and offers for new reservations:{bcolors.ENDC}\n")
 
@@ -100,6 +128,7 @@ def test_start():
     from sqlalchemy import func
 
     print(f"\n\n{bcolors.HEADER}#################### QUERY 2 ####################\n{bcolors.ENDC}")
+    t0 = time.time()
     bestoffer = db.session.query(
         Travel_agency_offer
     ).select_from(
@@ -113,6 +142,7 @@ def test_start():
     ).group_by(
         Travel_agency_offer
     ).order_by(func.count(Client.id).desc()).first()
+    t2 = time.time() - t0
 
     print(f"\n{bcolors.FAIL}Most popular offer among clients living in Warsaw: {bcolors.ENDC}" + str(bestoffer.serialize) + '\n')
 
@@ -130,7 +160,88 @@ def test_start():
     ).join(
         Travel_agency_offer, Reservation.offer_id == Travel_agency_offer.id
     ).all()
-    
+
     print(f"\n{bcolors.FAIL}List of offers and clients living in Warsaw: {bcolors.ENDC}")
     [print(row) for row in bestoffer]
+
+    return t1, t2
+
+def test_sql():
+    result = urlparse(os.getenv("DATABASE_URL"))
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+
+    connection = psycopg2.connect(
+        user=username,
+        password=password,
+        host=hostname,
+        port=port,
+        database=database
+        )
+
+    cursor = connection.cursor()
+
+    jfile = open("tests/offer.json", "r")
+    jobj = json.loads(jfile.read())
+
+    columns = list(jobj.keys())
+    print ("\ncolumn names:", columns)
+
+    table_name = "Travel_agency_offer"
+    insert_offer_str = 'INSERT INTO "{}" '.format( table_name )
+    insert_offer_str += "(" + ', '.join(columns) + ")\nVALUES "
+    values = []
+    for col_names, val in jobj.items():
+
+        # Postgres strings must be enclosed with single quotes
+        if type(val) == str:
+            # escape apostrophies with two single quotations
+            val = val.replace("'", "''")
+            val = "'" + val + "'"
+
+        values += [ str(val) ]
+
+    # join the list of values and enclose record in parenthesis
+    insert_offer_str += "(" + ', '.join(values) + "),\n"
+    # remove the last comma and end statement with a semicolon
+    insert_offer_str = insert_offer_str[:-2]
+
+    # table_name = "json_data"
+    # insert_offer_str = "INSERT INTO %s (%s)\nVALUES %s" % (
+    #     table_name,
+    #     ', '.join(columns),
+    #     values_str
+    # )
+    print(f"\n\n{bcolors.HEADER}#################### QUERY 1 ####################\n{bcolors.ENDC}")
+
+    print ("\nSQL statement:")
+    print (insert_offer_str)
+
+    t0 = time.time()
+    cursor.execute( f"""
+        WITH insert_query AS
+        ({insert_offer_str} RETURNING "Travel_agency_offer".id)
+         INSERT INTO "Reservation" (client_id, price, status, offer_id) SELECT "Client".id, 0, 'accepted', insert_query.id AS id_1
+        FROM insert_query, "User" JOIN "Client" ON "User".id = "Client".id JOIN ("User" AS "User_1" JOIN "Employee" AS "Employee_1" ON "User_1".id = "Employee_1".id) ON "User".uid = "User_1".uid
+        """)
+    connection.commit()
+    t1 = time.time() - t0
+
+    print(f"\n\n{bcolors.HEADER}#################### QUERY 2 ####################\n{bcolors.ENDC}")
+
+    t0 = time.time()
+    cursor.execute("""
+        SELECT "Travel_agency_offer".id AS "Travel_agency_offer_id", "Travel_agency_offer".uid AS "Travel_agency_offer_uid", "Travel_agency_offer".transport AS "Travel_agency_offer_transport", "Travel_agency_offer".accommodation AS "Travel_agency_offer_accommodation", "Travel_agency_offer".event AS "Travel_agency_offer_event", "Travel_agency_offer".organizer_id AS "Travel_agency_offer_organizer_id"
+        FROM "User" JOIN "Client" ON "User".id = "Client".id JOIN "Reservation" ON "Client".id = "Reservation".client_id JOIN "Travel_agency_offer" ON "Reservation".offer_id = "Travel_agency_offer".id
+        WHERE "Client".address = 'Warszawa' GROUP BY "Travel_agency_offer".id, "Travel_agency_offer".uid, "Travel_agency_offer".transport, "Travel_agency_offer".accommodation, "Travel_agency_offer".event, "Travel_agency_offer".organizer_id ORDER BY count("Client".id) DESC
+        LIMIT 1
+        """)
+    connection.commit()
+    t2 = time.time() - t0
+    print(cursor.fetchall())
+
+    return t1, t2
 
